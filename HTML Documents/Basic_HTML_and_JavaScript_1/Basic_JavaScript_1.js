09e36eb94e3b2cc465325de57895d5623b57cc16
henrystanaford-code <keonistanaford@gmail.com>
2026-04-07 09:52:02 -0400

    HTML and CSS Projects Major Update
    Upload of Exercises for The Tech Academy. Page 187 Relevant Files: Basic_HTML_5 + Basic_CSS_2

--- HTML Documents/Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ HTML Documents/Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,6 +1,6 @@
 function My_First_Function() {
+	alert("any text");
 		var String = "Kiss me, I'm Irish!";
 		var result = String.fontcolor("green");
-		document.getElementbyId("Irish").innerHTML = 
-result;
-	}
+		document.getElementById("Irish").innerHTML = result;
+}
